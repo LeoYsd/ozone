453cc7e784f71c694fde28fc096b6c426b6d29c5
Olumide Micheal <olumidemicheal7@gmail.com>
2023-05-08 14:53:15 +0100

    footer now responsive

--- src/component/common/footer/footer.js
+++ src/component/common/footer/footer.js
@@ -4,16 +4,18 @@ import { FiInstagram } from 'react-icons/fi';
 import BrandLogo from "../../../assets/images/OzoneLogo.png";
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
         <div className="grid place-items-center bg-[#58504A1A] w-full">
-            <div className="my-20 w-[90%] flex align-center justify-center">
-                <div className="w-[35%] flex flex-col gap-3">
+            <div className="my-20 w-[90%] flex align-center justify-center sm:flex-col-reverse md:flex-row md:gap-0 sm:gap-10">
+                <div className="md:w-[35%] flex flex-col gap-3">
                     <div>
                         <img src={BrandLogo} alt="logo"/>
                     </div>
                     <h3 className="font-bold capitalize leading-[28px] text-[1.2rem]">Contact us</h3>
                     <p className="text-[#6D737A]">Call : +123 400 123</p>
-                    <address className="text-[#6D737A] not-italic w-2/3">No 5. Ozumba Mbadiwe road 112110, Gwarinpa, Lagos State.</address>
+                    <address className="text-[#6D737A] not-italic md:w-2/3 sm:w-full">No 5. Ozumba Mbadiwe road 112110, Gwarinpa, Lagos State.</address>
                     <a href="mailto:example@example.com" className="font-bold">Email: example@example.com</a>
                     <div className="flex gap-2">
                         <div className="bg-[#fafafa] p-4">
@@ -33,9 +35,9 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-                <div className="w-[65%] flex items-center justify-between">
-                    <div className='w-[33%] flex justify-between flex-col h-full'>
-                        <h3 className='h-[10%] font-bold text-[#1B1D1F] text-[1.3rem]'>Features</h3>
+                <div className="w-[65%] flex md:flex-row sm:flex-col items-center justify-between sm:gap-12 md:gap-0">
+                    <div className='md:w-[33%] sm:w-full flex justify-between flex-col h-full'>
+                        <h3 className='h-[10%] font-bold text-[#1B1D1F] text-[1.3rem] sm:mb-3 md:mb-0'>Features</h3>
                         <ul className='h-[80%] text-[#6D737A] flex flex-col gap-2'>
                             <li>Home</li>
                             <li>Become a host</li>
@@ -44,8 +46,8 @@ const Footer = () => {
                             <li>Contact</li>
                         </ul>
                     </div>
-                    <div className='w-[33%] flex justify-between flex-col h-full'>
-                        <h3 className='h-[10%] font-bold text-[#1B1D1F] text-[1.3rem]'>Company</h3>
+                    <div className='md:w-[33%] sm:w-full flex justify-between flex-col h-full'>
+                        <h3 className='h-[10%] font-bold text-[#1B1D1F] text-[1.3rem] sm:mb-3 md:mb-0'>Company</h3>
                         <ul className='h-[80%] text-[#6D737A] flex flex-col gap-2'>
                             <li>About us</li>
                             <li>Press contact</li>
@@ -53,8 +55,8 @@ const Footer = () => {
                             <li>Blog</li>
                         </ul>
                     </div>
-                    <div className='w-[33%] flex justify-between flex-col h-full'>
-                        <h3 className='h-[10%] font-bold text-[#1B1D1F] text-[1.3rem]'>Team and Policies</h3>
+                    <div className='md:w-[33%] sm:w-full flex justify-between flex-col h-full'>
+                        <h3 className='h-[10%] font-bold text-[#1B1D1F] text-[1.3rem] sm:mb-3 md:mb-0'>Team and Policies</h3>
                         <ul className='h-[80%] text-[#6D737A] flex flex-col gap-2'>
                             <li>Terms of service</li>
                             <li>Privacy policy</li>
@@ -62,6 +64,9 @@ const Footer = () => {
                         </ul>
                     </div>
                 </div>
+            </div>
+            <div className='text-center text-white bg-dark py-3 w-full'>
+                &copy; {currentYear} Copyright Ozone.
             </div>
         </div>
     );
