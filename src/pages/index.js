1bd77d559734e4a534a540fc86607ceebd75d9b6
Olumide Micheal <olumidemicheal7@gmail.com>
2023-05-17 23:12:54 +0100

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,44 @@ import { TopList, WatchList } from "../template/Listings";
 import RecommendedProperties from "../template/Properties/recommendedProperties";
 
 const LandingPage = () => {
-    const [topListing, setTopListing] = useState(true);
-return (
+  const [topListing, setTopListing] = useState(true);
+  const [watchlist, setWatchlist] = useState([]);
+
+  const addToWatchlist = (listing) => {
+    setWatchlist((prevWatchlist) => [...prevWatchlist, listing]);
+  };
+
+  return (
     <LandingPageLayout>
-        <div className="my-9">
-            <div className="flex w-full md:justify-start md:gap-4 sm:justify-between cursor-pointer">
-                <p className={`font-bold trans border-b-2 ${topListing ? 'border-purple text-dark' : 'text-subText border-white'} py-2`} onClick={() => setTopListing(true)}>Top</p>
-                <p className={`font-bold trans border-b-2 ${!topListing ? 'border-purple text-dark' : 'text-subText border-white'} py-2`} onClick={() => setTopListing(false)}>WatchList</p>
-            </div>
-            <div className="w-full h-px bg-hack"></div>
-
-            {topListing ? (<TopList />) : (<WatchList />)}
+      <div className="my-9">
+        <div className="flex w-full md:justify-start md:gap-4 sm:justify-between cursor-pointer">
+          <p
+            className={`font-bold trans border-b-2 ${
+              topListing ? "border-purple text-dark" : "text-subText border-white"
+            } py-2`}
+            onClick={() => setTopListing(true)}
+          >
+            Top
+          </p>
+          <p
+            className={`font-bold trans border-b-2 ${
+              !topListing ? "border-purple text-dark" : "text-subText border-white"
+            } py-2`}
+            onClick={() => setTopListing(false)}
+          >
+            WatchList
+          </p>
         </div>
+        <div className="w-full h-px bg-hack"></div>
 
-        <div className="w-full my-36">
-            <RecommendedProperties />
-        </div>
+        {topListing ? <TopList addToWatchlist={addToWatchlist} /> : <WatchList addToWatchlist={addToWatchlist} />}
+      </div>
 
+      <div className="w-full my-36">
+        <RecommendedProperties />
+      </div>
     </LandingPageLayout>
-);
+  );
 };
 
 export default LandingPage;
--- src/template/Listings/index.js
+++ src/template/Listings/index.js
@@ -1,25 +1,89 @@
+import { useState } from "react";
 import ListCard from "../../component/layout/Cards";
 import { Listings } from "../../utils/constants/listings";
 
-export const TopList = () => (
-    <div>
-        <p className="my-5 text-subText font-robotoLight">Featured</p>
-        <div className="flex flex-wrap justify-between">
-            {Listings.map((list) => (
-                <ListCard key={list.id} houseImage={list.image} title={list.title} address={list.address} parkSpace={list.parkSpace} price={list.price} ownerImage={list.owner_img} ownerName={list.owner_name} bathub={list.bathub} meter={list.meter} />
-            ))}
-        </div>
-    </div>
-);
+export const TopList = ({ addToWatchlist }) => {
+  const [favorites, setFavorites] = useState([]);
 
-export const WatchList = () => (
+  const handleFavoriteClick = (id) => {
+    const updatedFavorites = [...favorites];
+    const index = updatedFavorites.indexOf(id);
+
+    if (index > -1) {
+      updatedFavorites.splice(index, 1); // Remove from favorites
+    } else {
+      updatedFavorites.push(id); // Add to favorites
+    }
+
+    setFavorites(updatedFavorites);
+  };
+
+  return (
     <div>
+      <p className="my-5 text-subText font-robotoLight">Featured</p>
+      <div className="flex flex-wrap justify-between">
+        {Listings.map((list) => (
+          <ListCard
+            key={list.id}
+            houseImage={list.image}
+            title={list.title}
+            address={list.address}
+            parkSpace={list.parkSpace}
+            price={list.price}
+            ownerImage={list.owner_img}
+            ownerName={list.owner_name}
+            bathub={list.bathub}
+            meter={list.meter}
+            isFavorite={favorites.includes(list.id)}
+            onFavoriteClick={() => handleFavoriteClick(list.id)}
+            addToWatchlist={addToWatchlist} // Pass addToWatchlist function to ListCard component
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const WatchList = ({ addToWatchlist }) => {
+    const [favorites, setFavorites] = useState([]);
+  
+    const handleFavoriteClick = (id) => {
+      const updatedFavorites = [...favorites];
+      const index = updatedFavorites.indexOf(id);
+  
+      if (index > -1) {
+        updatedFavorites.splice(index, 1); // Remove from favorites
+      } else {
+        updatedFavorites.push(id); // Add to favorites
+      }
+  
+      setFavorites(updatedFavorites);
+    };
+  
+    const favoriteListings = Listings.filter((list) => favorites.includes(list.id));
+  
+    return (
+      <div>
         <p className="my-5 text-subText font-robotoLight">My Watch</p>
         <div className="flex flex-wrap w-full justify-between">
-            {Listings.map((list) => (
-                <ListCard key={list.id} houseImage={list.image} title={list.title} address={list.address} parkSpace={list.parkSpace} price={list.price} ownerImage={list.owner_img} ownerName={list.owner_name} bathub={list.bathub} meter={list.meter} />
-            ))}
+          {favoriteListings.map((list) => (
+            <ListCard
+              key={list.id}
+              houseImage={list.image}
+              title={list.title}
+              address={list.address}
+              parkSpace={list.parkSpace}
+              price={list.price}
+              ownerImage={list.owner_img}
+              ownerName={list.owner_name}
+              bathub={list.bathub}
+              meter={list.meter}
+              isFavorite={favorites.includes(list.id)}
+              onFavoriteClick={() => handleFavoriteClick(list.id)}
+              addToWatchlist={addToWatchlist} // Pass addToWatchlist function to ListCard component
+            />
+          ))}
         </div>
-    </div>
-);
-
+      </div>
+    );
+};
